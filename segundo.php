5065f019be37e4340c63fdc41b8d0fbc682d0add
José Elías <elias@cubopanel.com>
2015-11-05 14:50:41 -0600

    Ejemplo
    Descripción del ejemplo

---
+++ segundo.php
@@ -0,0 +1,3 @@
+Morbi malesuada nulla nec purus convallis consequat. Vivamus id mollis quam. Morbi ac commodo nulla. In condimentum orci id nisl volutpat bibendum. Quisque commodo hendrerit lorem quis egestas. Maecenas quis.
+
+ok
